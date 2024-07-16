666bf943a30ada311056924b4c5dc5e6fecb8c16
on22y <lucy14lee@gmail.com>
2024-07-16 20:48:06 +0900

    App 주석

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,34 +1,38 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/home/Home';
-import NavbarTop from './components/Navbar/NavbarTop';
-import NavbarBottom from './components/Navbar/NavbarBottom';
-import BoardBuy from './components/board/BoardBuy';
-import Signup from './components/mypage/Signup.js';
-import Login from './components/mypage/Login.js';
-import Mypage from './components/mypage/Mypage.js';
-import MypageEdit from './components/mypage/MypageEdit.js';
+// import React, { useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import Home from './components/home/Home';
+// import NavbarTop from './components/Navbar/NavbarTop';
+// import NavbarBottom from './components/Navbar/NavbarBottom';
+// import BoardBuy from './components/board/BoardBuy';
+// import Signup from './components/mypage/Signup.js';
+// import Login from './components/mypage/Login.js';
+// import Mypage from './components/mypage/Mypage.js';
+// import MypageEdit from './components/mypage/MypageEdit.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <NavbarTop />
-        </div>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/buy" element={<BoardBuy />} />
-          </Routes>
-        </div>
-        <div>
-          <NavbarBottom />
-        </div>
-      </Router>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <div>
+//           <NavbarTop />
+//         </div>
+//         <div className="content">
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/buy" element={<BoardBuy />} />
+// <Route path="/signuppage" element={<Signup />} />
+// <Route path="/loginpage" element={<Login />} />
+// <Route path="/mypage" element={<Mypage />} />
+// <Route path="/mypage/edit" element={MypageEdit />} />
+//           </Routes>
+//         </div>
+//         <div>
+//           <NavbarBottom />
+//         </div>
+//       </Router>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
