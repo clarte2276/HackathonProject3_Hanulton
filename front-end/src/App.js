e205e0c6fe2f46dfb228cd92e589fe397e7b46aa
eae22 <eun0110@dgu.ac.kr>
2024-07-16 20:43:05 +0900

    잠깐 편집

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/home/Home';
-import NavbarTop from './components/Navbar/NavbarTop';
 import NavbarBottom from './components/Navbar/NavbarBottom';
 import BoardBuy from './components/board/BoardBuy';
 import Signup from './components/mypage/Signup.js';
@@ -14,9 +13,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <NavbarTop />
-        </div>
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
