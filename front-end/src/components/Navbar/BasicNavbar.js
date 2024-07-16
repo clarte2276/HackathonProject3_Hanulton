9dde2954bc19ce225e82a336dbe8062493493e0a
on22y <lucy14lee@gmail.com>
2024-07-16 22:14:58 +0900

    mypage 컴포넌트 위치 수정

--- front-end/src/components/Navbar/BasicNavbar.js
+++ front-end/src/components/Navbar/BasicNavbar.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './BasicNavbar.css';
 
 function BasicNavbar(props) {
-  return <div>{props.title}</div>;
+  return (
+    <div className="navbar">
+      <h1>{props.title}</h1>
+      <div className="underline"></div>
+    </div>
+  );
 }
 
 export default BasicNavbar;
