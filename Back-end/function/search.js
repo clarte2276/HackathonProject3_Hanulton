3e14182cceaf4c41e591376845d04fd7192b903d
jei1126 <jei1126@dgu.ac.kr>
2024-07-16 21:34:18 +0900

    검색기능추가

--- Back-end/function/search.js
+++ Back-end/function/search.js
@@ -1,4 +1,3 @@
-//게시판 검색 기능
 const express = require('express');
 const mysql = require('mysql');
 const db_config = require('../config/db_config.json');
@@ -33,34 +32,51 @@ const executeQuery = (query, params, res, callback) => {
     });
   };
   
-  // 게시판 검색 기능
-  const searchBoard = (tableName, keyword, res) => {
-    const query = `SELECT no, title, nickname, content, DATE_FORMAT(created_date, '%Y년 %m월 %d일 %H시 %i분') AS created_date, state FROM ${tableName} WHERE title LIKE ? OR content LIKE ?`;
-    const searchKeyword = `%${keyword}%`;
-    executeQuery(query, [searchKeyword, searchKeyword], res, (results) => {
-      if (results.length === 0) {
-        res.status(404).json({ message: '검색된 게시물이 없습니다.' });
-      } else {
-        res.json(results);
-      }
-    });
-  };
-  
-  // 각각의 게시판 검색 라우터 생성
-  const createSearchRoutes = (boardName, tableName) => {
-    // 검색
-    router.get(`/${boardName}/search`, (req, res) => {
-      const { keyword } = req.query;
-      if (!keyword) {
-        return res.status(400).send('검색어를 입력하세요.');
-      }
-      searchBoard(tableName, keyword, res);
-    });
-  };
-  
-  // 각각의 게시판 검색 라우트 설정
-  createSearchRoutes('boardbuy', 'boardbuy');
-  createSearchRoutes('boardsell', 'boardsell');
-  createSearchRoutes('boardads', 'boardads');
-  
-  module.exports = router;
+// 게시판 검색 기능
+const searchBoard = (tableName, keyword, res) => {
+  const query = `SELECT no, title, nickname, content, DATE_FORMAT(created_date, '%Y년 %m월 %d일 %H시 %i분') AS created_date, state, originprice, sellprice FROM ${tableName} WHERE title LIKE ? OR content LIKE ? OR nickname LIKE ?`;
+  const searchKeyword = `%${keyword}%`;
+  executeQuery(query, [searchKeyword, searchKeyword, searchKeyword], res, (results) => {
+    if (results.length === 0) {
+      res.status(404).json({ message: '검색된 게시물이 없습니다.' });
+    } else {
+      res.json(results);
+    }
+  });
+};
+
+// 게시판 목록 가져오기 기능
+const getBoardList = (tableName, res) => {
+  const query = `SELECT no, title, nickname, content, DATE_FORMAT(created_date, '%Y년 %m월 %d일 %H시 %i분') AS created_date, state, originprice, sellprice FROM ${tableName}`;
+  executeQuery(query, [], res, (results) => {
+    if (results.length === 0) {
+      res.status(404).json({ message: '게시물이 없습니다.' });
+    } else {
+      res.json(results);
+    }
+  });
+};
+
+// 각각의 게시판 검색 라우터 생성
+const createSearchRoutes = (boardName, tableName) => {
+  // 검색
+  router.get(`/${boardName}/search`, (req, res) => {
+    const { keyword } = req.query;
+    if (!keyword) {
+      return res.status(400).send('검색어를 입력하세요.');
+    }
+    searchBoard(tableName, keyword, res);
+  });
+
+  // 게시판 목록 가져오기
+  router.get(`/${boardName}`, (req, res) => {
+    getBoardList(tableName, res);
+  });
+};
+
+// 각각의 게시판 검색 및 목록 가져오기 라우트 설정
+createSearchRoutes('boardbuy', 'boardbuy');
+createSearchRoutes('boardsell', 'boardsell');
+createSearchRoutes('boardads', 'boardads');
+
+module.exports = router;
