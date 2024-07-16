0c18ecfd5a1dcd30a03e88bf664c68ae361699b5
jei1126 <jei1126@dgu.ac.kr>
2024-07-16 22:23:42 +0900

    get->post

--- Back-end/function/board.js
+++ Back-end/function/board.js
@@ -193,7 +193,7 @@ const updatePostState = (tableName, postId, newState, req, res) => {
 // 각각의 게시판 라우터 생성
 const createBoardRoutes = (boardName, tableName) => {
   // 게시판 데이터 가져오기
-  router.get(`/${boardName}`, (req, res) => {
+  router.post(`/${boardName}`, (req, res) => {
     getBoardData(tableName, res);
   });
 
