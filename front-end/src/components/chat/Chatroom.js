0cb82fa6de277739d44e353f640c7badd81d7d3a
on22y <lucy14lee@gmail.com>
2024-07-17 18:28:24 +0900

    채팅방

--- front-end/src/components/chat/Chatroom.js
+++ front-end/src/components/chat/Chatroom.js
@@ -1,32 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import BasicNavbar from '../Navbar/BasicNavbar';
+import './Chatroom.css';
 
 const Chatroom = () => {
   const { sender, receiver } = useParams();
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
+  const [newMessage, setNewMessage] = useState('');
   const [roomMessages, setRoomMessages] = useState([]);
 
   const fetchMessages = async () => {
     try {
-      console.log(
-        `Fetching messages for sender ${sender} to receiver ${receiver}`
-      );
-      const response = await axios.get(
-        `/chat/chatroom/${sender}/to/${receiver}/messages`
-      );
-      console.log("Fetched messages:", response.data);
+      console.log(`Fetching messages for sender ${sender} to receiver ${receiver}`);
+      const response = await axios.get(`/chat/chatroom/${sender}/to/${receiver}/messages`);
+      console.log('Fetched messages:', response.data);
       if (Array.isArray(response.data)) {
         const processedMessages = response.data.map((msg) => ({
           ...msg,
         }));
         setMessages(processedMessages);
       } else {
-        console.error("Fetched data is not an array:", response.data);
+        console.error('Fetched data is not an array:', response.data);
       }
     } catch (error) {
-      console.error("Error fetching messages:", error);
+      console.error('Error fetching messages:', error);
     }
   };
 
@@ -40,7 +38,7 @@ const Chatroom = () => {
   }, [sender, receiver]);
 
   useEffect(() => {
-    console.log("Messages state updated:", messages);
+    console.log('Messages state updated:', messages);
     if (Array.isArray(messages) && messages.length) {
       setRoomMessages(
         messages.filter(
@@ -53,9 +51,9 @@ const Chatroom = () => {
   }, [messages, receiver, sender]);
 
   const handleSendMessage = async () => {
-    if (newMessage.trim() === "") return;
+    if (newMessage.trim() === '') return;
     try {
-      console.log("Sending message:", {
+      console.log('Sending message:', {
         sender,
         receiver,
         content: newMessage,
@@ -63,18 +61,19 @@ const Chatroom = () => {
       const sender_id = sender;
       const receiver_id = receiver;
 
-      console.log("Sending message:", {
+      console.log('Sending message:', {
         sender_id,
         receiver_id,
         content: newMessage,
       });
 
       //백엔드에 보내기
-      const response = await axios.post(
-        `/chat/chatroom/${sender}/to/${receiver}/messages`,
-        { sender, receiver, content: newMessage }
-      );
-      console.log("Response:", response);
+      const response = await axios.post(`/chat/chatroom/${sender}/to/${receiver}/messages`, {
+        sender,
+        receiver,
+        content: newMessage,
+      });
+      console.log('Response:', response);
 
       const newMsg = {
         sender_id,
@@ -83,33 +82,38 @@ const Chatroom = () => {
       };
 
       setMessages((prevMessages) => [...prevMessages, newMsg]);
-      setNewMessage("");
+      setNewMessage('');
     } catch (error) {
-      console.error("Error sending message:", error);
+      console.error('Error sending message:', error);
     }
   };
 
   return (
-    <div className="chattingRoom">
-      <div className="chatroom-messages">
-        {roomMessages.map((msg, index) => (
-          <div key={index}>
-            <p>{msg.content}</p>
-          </div>
-        ))}
-      </div>
-      <div className="msgsendItem">
-        <div className="inputMessage">
-          <input
-            type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            placeholder="Typing your message"
-          />
+    <div className="chattingRoomAll">
+      <BasicNavbar title="채팅" />
+      <div className="chattingRoom">
+        <div className="chatroom-messages">
+          {roomMessages.map((msg, index) => (
+            <div key={index}>
+              <p>{msg.content}</p>
+            </div>
+          ))}
         </div>
-        <button onClick={handleSendMessage} className="sendmessageBtn">
-          Send
-        </button>
+        <div className="msgsendItem">
+          <div className="inputMessage">
+            <input
+              type="text"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Typing your message"
+            />
+          </div>
+          <div className="sendBtnContent">
+            <button onClick={handleSendMessage} className="sendmessageBtn">
+              Send
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
