40af3d22a8083d7e46fdc5a0bb791c8a1227fe9d
eae22 <eun0110@dgu.ac.kr>
2024-07-17 18:30:24 +0900

    막

--- front-end/src/components/board/CRUD/Cook_friendCreate.js
+++ front-end/src/components/board/CRUD/Cook_friendCreate.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import BasicNavbar from "../../Navbar/BasicNavbar";
-import "./CRUD.css";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import BasicNavbar from '../../Navbar/BasicNavbar';
+import './CRUD.css';
+import axios from 'axios';
 
 function Cook_friendCreate() {
   const navigate = useNavigate();
@@ -10,14 +10,13 @@ function Cook_friendCreate() {
   const { nextNo, gptResponse } = location.state || {};
 
   const [board, setBoard] = useState({
-    title: "",
-    sellprice: "",
-    originprice: "",
-    body: gptResponse || "", // gptResponse를 초기값으로 설정
+    title: '',
+    place: '',
+    body: '',
   });
   const [file, setFile] = useState(null); // 파일 상태 추가
 
-  const { title, body, sellprice, originprice } = board;
+  const { title, body, place } = board;
 
   const onChange = (event) => {
     const { value, name } = event.target;
@@ -35,123 +34,120 @@ function Cook_friendCreate() {
     event.preventDefault();
 
     const formData = new FormData();
-    formData.append("file", file); // 파일 추가
-    formData.append("no", nextNo);
-    formData.append("title", title);
-    formData.append("content", body);
-    formData.append("created_date", new Date().toISOString());
-    formData.append("originprice", originprice);
-    formData.append("sellprice", sellprice);
+    formData.append('file', file); // 파일 추가
+    formData.append('no', nextNo);
+    formData.append('title', title);
+    formData.append('content', body);
+    formData.append('created_date', new Date().toISOString());
+    formData.append('place', place);
 
     try {
-      const response = await axios.post(
-        `/boardcookfriend/process/new_Post`,
-        formData,
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const response = await axios.post(`/boardcookfriend/process/new_Post`, formData, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
       const newPostId = response.data.no;
-      console.log("왜안돼", { newPostId }, response.data);
-      alert("등록되었습니다.");
+      alert('등록되었습니다.');
       navigate(`/boardcookfriend`, { state: { newPostId } });
     } catch (error) {
-      console.error("Error saving post:", error);
-      alert("글을 저장하는 도중 오류가 발생했습니다.");
+      console.error('Error saving post:', error);
+      alert('글을 저장하는 도중 오류가 발생했습니다.');
     }
   };
 
   const backToList = () => {
-    navigate("/boardcookfriend");
+    navigate('/boardcookfriend');
+  };
+
+  const getBoard = () => {
+    switch (location.pathname) {
+      case '/boardsell/process/new_Post':
+        return '우리요리';
+      case '/boardcookfriend/process/new_Post':
+        return '레시피공유';
+      default:
+        return '';
+    }
   };
 
   return (
-    <div className="Create_all">
-      <div>
-        <div>
-          <BasicNavbar title="글쓰기" />
+    <>
+      <BasicNavbar title="글쓰기" />
+      <div className="Create_all">
+        <div className="boardName_layout">
+          <div className="boardTitle">게시판</div>
+          <div className="boardName">{getBoard()}</div>
         </div>
         <form onSubmit={saveBoard}>
-          <div className="titleBody_layout">
-            <div>
+          <div className="BoardInput_all">
+            <div className="titleBody_layout">
               <p>
-                <span className="titleBody_name">제목</span>
-              </p>
-              <input
-                className="titleInput"
-                type="text"
-                name="title"
-                placeholder="제목을 입력하세요"
-                value={title}
-                onChange={onChange}
-              />
-            </div>
-            <div>
-              <div>
-                <p>
-                  <span className="titleBody_name">판매 가격</span>
+                <p className="titleBody_nameLayout">
+                  <span className="titleBody_name">제목</span>
                 </p>
                 <input
-                  className="sellpriceInput"
+                  className="titleInput"
                   type="text"
-                  name="sellprice"
-                  placeholder="판매 가격"
-                  value={sellprice}
+                  name="title"
+                  placeholder="제목을 입력하세요"
+                  value={title}
                   onChange={onChange}
                 />
+              </p>
+            </div>
+            <br />
+            <div className="titleBody_layout">
+              <div className="Body_layout">
+                <p className="titleBody_nameLayout">
+                  <span className="titleBody_name">글내용</span>
+                </p>
+                <textarea
+                  className="BodyInput"
+                  name="body"
+                  placeholder="내용을 입력하세요"
+                  value={body}
+                  onChange={onChange}
+                ></textarea>
               </div>
+            </div>
+            <br />
+            <div className="titleBody_layout">
               <div>
-                <p>
-                  <span className="titleBody_name">정가</span>
+                <p className="titleBody_nameLayout">
+                  <span className="titleBody_name">사진 업로드</span>
+                </p>
+                <input className="imgUp" type="file" onChange={onFileChange} />
+              </div>
+            </div>
+            <br />
+            <div className="titleBody_layout">
+              <p>
+                <p className="titleBody_nameLayout">
+                  <span className="titleBody_name">요리 희망 장소</span>
                 </p>
                 <input
-                  className="originpriceInput"
+                  className="titleInput"
                   type="text"
-                  name="originprice"
-                  placeholder="정가"
-                  value={originprice}
+                  name="place"
+                  placeholder="장소를 입력하세요"
+                  value={place}
                   onChange={onChange}
                 />
-              </div>
-            </div>
-          </div>
-          <br />
-          <div className="titleBody_layout">
-            <div>
-              <p>
-                <span className="titleBody_name">글내용</span>
               </p>
-              <textarea
-                className="BodyInput"
-                name="body"
-                placeholder="내용을 입력하세요"
-                value={body}
-                onChange={onChange}
-              ></textarea>
             </div>
-          </div>
-          <br />
-          <div className="titleBody_layout">
-            <div>
-              <p>
-                <span className="titleBody_name">이미지 업로드</span>
-              </p>
-              <input type="file" onChange={onFileChange} />
+            <br />
+            <div className="btn_layout">
+              <button className="backBtn" type="button" onClick={backToList}>
+                취소
+              </button>
+              <input className="CreateBtn" type="submit" value="작성완료"></input>
             </div>
-          </div>
-          <br />
-          <div className="btn_layout">
-            <button className="backBtn" type="button" onClick={backToList}>
-              취소
-            </button>
-            <input className="CreateBtn" type="submit" value="작성완료"></input>
           </div>
         </form>
       </div>
-    </div>
+    </>
   );
 }
 
--- front-end/src/components/board/CRUD/Cook_friendRead.js
+++ front-end/src/components/board/CRUD/Cook_friendRead.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
-import "./CRUD.css";
+import React, { useState, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import NavbarTop from '../../Navbar/NavbarTop';
+import axios from 'axios';
+import './CRUD.css';
 
 function Cook_friendRead() {
   const { no } = useParams();
@@ -16,11 +17,10 @@ function Cook_friendRead() {
       try {
         const postResponse = await axios.get(`/boardcookfriend/PostView/${no}`);
         setPost(postResponse.data.post);
-
         setLoading(false);
       } catch (error) {
-        console.error("게시글을 불러오는 중 오류 발생:", error);
-        setError("게시글을 불러오는 중 오류 발생");
+        console.error('게시글을 불러오는 중 오류 발생:', error);
+        setError('게시글을 불러오는 중 오류 발생');
         setLoading(false);
       }
     };
@@ -29,19 +29,19 @@ function Cook_friendRead() {
   }, [no]);
 
   const handleDelete = async () => {
-    const confirmDelete = window.confirm("정말로 삭제하시겠습니까?");
+    const confirmDelete = window.confirm('정말로 삭제하시겠습니까?');
 
     if (confirmDelete) {
       try {
         await axios.delete(`/boardcookfriend/Postview/${no}/process/delete`);
-        alert("게시글이 삭제되었습니다.");
-        navigate("/boardcookfriend");
+        alert('게시글이 삭제되었습니다.');
+        navigate('/boardcookfriend');
       } catch (error) {
         if (error.response && error.response.status === 403) {
-          alert("삭제 권한이 없습니다.");
+          alert('삭제 권한이 없습니다.');
         } else {
-          console.error("게시글 삭제 중 오류 발생:", error);
-          alert("게시글 삭제 중 오류가 발생했습니다.");
+          console.error('게시글 삭제 중 오류 발생:', error);
+          alert('게시글 삭제 중 오류가 발생했습니다.');
         }
       }
     }
@@ -59,76 +59,43 @@ function Cook_friendRead() {
     return <div>게시글을 찾을 수 없습니다.</div>;
   }
 
-  const {
-    title,
-    nickname,
-    created_date,
-    content,
-    sellprice,
-    originprice,
-    file_path,
-  } = post;
-  const discountRate =
-    originprice && sellprice
-      ? ((1 - sellprice / originprice) * 100).toFixed(2)
-      : 0;
+  const { title, nickname, created_date, content, place } = post;
 
   return (
-    <div className="Read_all">
-      <div>
-        <div className="ReadTitle">{title}</div>
-        <div className="infoUpdateDelete">
-          <div className="info">
-            <p>닉네임 : {nickname}</p>
-            <p>{created_date}</p>
+    <>
+      <NavbarTop />
+      <div className="Read_alltwo">
+        <div className="Read_allone">
+          <div className="imageContainer">
+            <img src={`/boardsell/image/${no}`} alt="Uploaded" style={{ maxWidth: '80%', height: 'auto' }} />
           </div>
-          <div className="updateDelete">
-            <Link
-              className="deleteUpdate"
-              to={`/boardcookfriend/Postview/${no}/process/update`}
-            >
-              수정
-            </Link>
-            <div
-              className="deleteUpdate"
-              onClick={handleDelete}
-              style={{ cursor: "pointer" }}
-            >
-              삭제
+          <div className="nicknamePlace_layout">
+            <p className="sellRead_location">{place}</p>
+            <p className="sellRead_nickname">닉네임 : {nickname}</p>
+          </div>
+          <div className="sellRead_underline"></div>
+          <div className="Readboard_info">
+            <div className="ReadTitleDate">
+              <div className="ReadTitle">{title}</div>
+              <div className="info">
+                <p className="Read_date_">{created_date}</p>
+              </div>
+            </div>
+            <div className="updateDelete">
+              <Link className="deleteUpdate" to={`/boardsell/Postview/${no}/process/update`}>
+                수정
+              </Link>
+              <div className="deleteUpdate" onClick={handleDelete} style={{ cursor: 'pointer' }}>
+                삭제
+              </div>
             </div>
           </div>
-        </div>
-        {file_path && (
-          <div className="imageContainer">
-            <img
-              src={`/${file_path.replace(/\\/g, "/")}`}
-              alt="Uploaded"
-              style={{ maxWidth: "100%", height: "auto" }}
-            />
-          </div>
-        )}
-        <div>
-          <div>판매 가격 : {sellprice}</div>
-          <div>
-            <div>정가 : {originprice}</div>
-            <div>할인율 : {discountRate}%</div>
-          </div>
-        </div>
-        <div className="ReadContent">{content}</div>
-        <div>
-          <div>
-            <Link className="buyChange" to={"/"}>
-              바로구매
-            </Link>
-          </div>
-          <div>
-            <Link className="buyChange" to={"/"}>
-              교환신청
-            </Link>
+          <div className="Readcontent_layout">
+            <div className="ReadContent">{content}</div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
