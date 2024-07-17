4a0fe6690330ac38f7bed4a8ca2dc3f8bf6f251c
eae22 <eun0110@dgu.ac.kr>
2024-07-17 16:42:06 +0900

    업데이트 place

--- front-end/src/components/board/CRUD/SellUpdate.js
+++ front-end/src/components/board/CRUD/SellUpdate.js
@@ -1,19 +1,18 @@
 // export default SellUpdate;
 
-import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import BasicNavbar from "../../Navbar/BasicNavbar";
-import "./CRUD.css";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import BasicNavbar from '../../Navbar/BasicNavbar';
+import './CRUD.css';
+import axios from 'axios';
 
 function SellUpdate() {
   const { no } = useParams();
   const navigate = useNavigate();
   const [post, setPost] = useState({
-    title: "",
-    sellprice: "",
-    originprice: "",
-    body: "",
+    title: '',
+    body: '',
+    place: '',
   });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -23,19 +22,18 @@ function SellUpdate() {
     axios
       .get(`/boardsell/Postview/${no}/process/update`)
       .then((response) => {
-        const { title, sellprice, originprice, content, file_path } =
-          response.data;
+        const { title, content, place, file_path } = response.data;
 
-        setPost({ title, sellprice, originprice, body: content });
+        setPost({ title, body, place: content });
         setLoading(false);
       })
       .catch((error) => {
         if (error.response && error.response.status === 403) {
-          alert("수정 권한이 없습니다.");
+          alert('수정 권한이 없습니다.');
           navigate(`/boardsell/PostView/${no}`);
         } else {
-          console.error("게시글을 불러오는 중 오류가 발생했습니다!", error);
-          setError("게시글을 불러오는 중 오류가 발생했습니다!");
+          console.error('게시글을 불러오는 중 오류가 발생했습니다!', error);
+          setError('게시글을 불러오는 중 오류가 발생했습니다!');
           setLoading(false);
         }
       });
@@ -59,28 +57,27 @@ function SellUpdate() {
 
     const formData = new FormData();
 
-    formData.append("file", file);
-    formData.append("title", post.title);
-    formData.append("content", post.body);
-    formData.append("sellprice", post.sellprice);
-    formData.append("originprice", post.originprice);
+    formData.append('file', file);
+    formData.append('title', post.title);
+    formData.append('content', post.body);
+    formData.append('place', post.place);
 
     try {
       await axios.post(`/boardsell/Postview/${no}/process/update`, formData, {
         withCredentials: true,
         headers: {
-          "Content-Type": "multipart/form-data",
+          'Content-Type': 'multipart/form-data',
         },
       });
-      alert("수정되었습니다.");
+      alert('수정되었습니다.');
       navigate(`/boardsell/PostView/${no}`);
     } catch (error) {
       if (error.response && error.response.status === 403) {
-        alert("수정 권한이 없습니다.");
+        alert('수정 권한이 없습니다.');
         navigate(`/boardsell/PostView/${no}`);
       } else {
-        console.error("게시글을 수정하는 중 오류가 발생했습니다:", error);
-        alert("게시글을 수정하는 중 오류가 발생했습니다:");
+        console.error('게시글을 수정하는 중 오류가 발생했습니다:', error);
+        alert('게시글을 수정하는 중 오류가 발생했습니다:');
       }
     }
   };
@@ -105,45 +102,19 @@ function SellUpdate() {
       <form onSubmit={updatePost}>
         <div>
           <span>제목</span>
-          <input
-            type="text"
-            name="title"
-            placeholder="제목"
-            value={post.title}
-            onChange={onChange}
-          />
+          <input type="text" name="title" placeholder="제목" value={post.title} onChange={onChange} />
         </div>
         <br />
-        <div>
-          <span>판매 가격</span>
-          <input
-            type="text"
-            name="sellprice"
-            placeholder="판매 가격"
-            value={post.sellprice}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <span>정가</span>
-          <input
-            type="text"
-            name="originprice"
-            placeholder="정가"
-            value={post.originprice}
-            onChange={onChange}
-          />
-        </div>
         <div>
           <span>내용</span>
-          <textarea
-            name="body"
-            placeholder="내용"
-            value={post.body}
-            onChange={onChange}
-          ></textarea>
+          <textarea name="body" placeholder="내용" value={post.body} onChange={onChange}></textarea>
         </div>
         <br />
+        <div>
+          <span>요리 희망 장소</span>
+          <input type="text" name="place" placeholder="장소" value={post.place} onChange={onChange} />
+        </div>
+
         <div>
           <span>이미지 업로드</span>
           <input type="file" onChange={onFileChange} />
